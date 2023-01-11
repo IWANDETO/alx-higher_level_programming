81441c32f35528de71fea59a090711aefaa40cdd
IWANDETO <wandettoian@gmail.com>
2023-01-11 11:04:25 +0000

    more errors

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def read_file(filename = ""):
+def read_file(filename=""):
 	"""
 		This function reads a text file and prints it out on the screen
 	"""
-	with open(filname, mode = "r" encoding = "utf-8") as my_file:
-		print(my_file.read(), end = "")
+	with open(filname, mode="r" encoding="utf-8") as my_file:
+		print(my_file.read(), end="")

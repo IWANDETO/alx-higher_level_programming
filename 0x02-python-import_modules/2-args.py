6675786e12842ebe64a2d31937f0a390cd911e4d
IWANDETO <wandettoian@gmail.com>
2023-03-09 17:33:33 +0300

    exe

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,13 @@ if __name__ == "__main__":
     from sys import argv
     number = len(argv)
     if len(argv) == 1:
-        print("{:d} .".format(len(argv)))
+        print("{:d} .".format(number))
     elif len(argv) == 2:
-        print("{:d} argument:".format(len(argv - 1)))
+        print("{:d} argument:".format(number - 1))
     else:
-        print("{:d} arguments:".format(len(argv - 1)))
+        print("{:d} arguments:".format(number - 1))
     for i in range(1, number):
-        print("{:d}: {:d}".format(i, (argv[i])))
+        print("{:d}: {:s}".format(i, (argv[i])))
 
 
    
